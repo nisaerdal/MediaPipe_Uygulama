f15c1e5a96bb427c1d3ebb5dd3c30b4a6ccb2612
Nisanur Erdal <67099685+nisaerdal@users.noreply.github.com>
2023-02-02 04:22:29 +0300

    Update hands_detection.py

--- hands_detection.py
+++ hands_detection.py
@@ -2,20 +2,20 @@ import cv2
 import mediapipe as mp
 import time
 
-class HandsDetection():
+class HandsDetection:
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
-        self.mode=mode
-        self.maxHands=maxHands
-        self.detectionCon=detectionCon
-        self.trackCon=trackCon
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands=self.mpHands.Hands(self.mode, self.maxHands,
-                                      self.detectionCon, self.trackCon)
-        self.mpDraw=mp.solutions.drawing_utils
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, image, draw=True):
-        imgRGB = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+        imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = self.hands.process(imgRGB)
 
         if results.multi_hand_landmarks:
@@ -26,8 +26,8 @@ class HandsDetection():
         return image
 
 def main():
-    pTime = 0
-    cTime= 0
+    prev_time = 0
+    curr_time = 0
 
     cap = cv2.VideoCapture(0)
     detector = HandsDetection()
@@ -35,11 +35,11 @@ def main():
     while True:
         success, image = cap.read()
         image = detector.findHands(image)
-        cTime = time.time()
-        fps = 1 / (cTime-pTime)
-        pTime = cTime
+        curr_time = time.time()
+        fps = 1 / (curr_time - prev_time)
+        prev_time = curr_time
 
-        cv2.putText(image, str(int(fps)), (10,70),
+        cv2.putText(image, str(int(fps)), (10, 70),
                     cv2.FONT_HERSHEY_PLAIN, 3,
                     (255, 0, 255), 3)
 
@@ -48,6 +48,3 @@ def main():
             cv2.destroyAllWindows()
             cap.release()
             break
-
-
-
