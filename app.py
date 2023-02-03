a1aadff510136a9c17bd6add0911df0d7b4e1317
nisaerdal <nisanerdl@hotmail.com>
2023-02-03 06:19:14 +0300

    update

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
+from hands_detection import HandsDetection
+#from pose_detection import PoseDetection
+#from holistic_detection import HolisticDetection
+
 def main():
     choise_input = input("1:Hands Detection, 2:Pose Detection, 3:Holistic Detection =>")
 
     if choise_input == '1':
-        from hands_detection import HandsDetection
         detector = HandsDetection()
-        #hands_detector.run_detection()
 
     elif choise_input == '2':
-        from pose_detection import PoseDetection
-        detector = PoseDetection()
-
-    elif choise_input == '3':
+        #detector = PoseDetection()
         pass
-
+    elif choise_input == '3':
+        #detector = HolisticDetection()
+        pass
     else:
         print('Tekrar input gir lütfen!')
 
